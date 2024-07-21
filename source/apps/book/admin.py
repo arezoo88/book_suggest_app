b9e2c3c80c9f2af61d7ca2ea857d779c99a6c5bd
Arezoo <arezoo.darvishi69@yahoo.com>
2024-07-21 17:19:16 +0330

    customize admin for book app

--- source/apps/book/admin.py
+++ source/apps/book/admin.py
@@ -1,1 +1,16 @@
 from django.contrib import admin
+from apps.book.models import Book, Review
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'author')
+    list_filter = ('genre',)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'rating')
+    list_filter = ('rating',)
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Review, ReviewAdmin)
